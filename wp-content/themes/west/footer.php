860f88baa38e52ad95a320a42a888d35f9b338fd
sonnguyen89 <nam_son14@yahoo.com>
2018-06-09 10:41:21 +1000

    update footer

--- wp-content/themes/west/footer.php
+++ wp-content/themes/west/footer.php
@@ -16,14 +16,6 @@
 	<?php endif; ?>
 
     <a class="go-top"><i class="fa fa-angle-up"></i></a>
-
-	<footer id="colophon" class="site-footer" role="contentinfo">
-		<div class="site-info container">
-			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'west' ) ); ?>"><?php printf( esc_html__( 'Proudly powered by %s', 'west' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( esc_html__( 'Theme: %2$s by %1$s.', 'west' ), 'aThemes', '<a href="http://athemes.com/theme/west" rel="designer">West</a>' ); ?>
-		</div><!-- .site-info -->
-	</footer><!-- #colophon -->
 </div><!-- #page -->
 
 <?php wp_footer(); ?>
