876890f1381fdfb72ac5c39ea243442e0ddb6b59
sonnguyen89 <nam_son14@yahoo.com>
2019-02-06 13:14:31 +1100

    update syle

--- wp-content/themes/west/footer.php
+++ wp-content/themes/west/footer.php
@@ -20,5 +20,16 @@
 
 <?php wp_footer(); ?>
 
+<div class="footer-meta">
+    <div class="container">
+        <div class="row">
+            <div class="copyright col-sm-12">
+                <span>Copyright © 2019 Caravan Sales Australia</span>
+                <span class="notice">Terms and Conditions Apply. Limited to advertised stock only. Offer expires 5pm (AEDT) February 9th 2019. Payment in full must be made before the offer expires.</span>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
